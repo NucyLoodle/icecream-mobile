d352f7a8664c133e717632fd9558156e8db4b16b
NucyLoodle <lucymeredith3@gmail.com>
2025-03-14 07:29:49 +0000

    edit splashscreen

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import * as dotenv from 'dotenv';
 import { Text, View,  StyleSheet } from 'react-native';
 import { Link } from 'expo-router';
@@ -6,32 +7,51 @@ import {
   useFonts,
   AlfaSlabOne_400Regular,
 } from "@expo-google-fonts/alfa-slab-one";
+import * as SplashScreen from 'expo-splash-screen';
 
 import {
   Poppins_400Regular,
 }  from "@expo-google-fonts/poppins";
 
 
-export default function Index() {
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
-  let [fontsLoaded] = useFonts({
-    AlfaSlabOne_400Regular
+// Set the animation options. This is optional.
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
+
+export default function Index() {
+  const [fontsLoaded] = useFonts({
+    AlfaSlabOne_400Regular,
   });
 
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      // Hide the splash screen once fonts are loaded
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
   if (!fontsLoaded) {
-    return 
-  } else {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Icecream Tracker</Text>
-        <Link href="/TrackVan" style={styles.button}>
-        Go to Track Van screen
-      </Link>
-      </View>
-    );
+    return null; // Keep splash screen visible while loading fonts
   }
 
+  return (
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={styles.text}>Icecream Tracker</Text>
+
+      
+      <Link href="/TrackVan" style={styles.button}>
+      Go to Track Van screen
+    </Link>
+    </View>
+  );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
