07abb770197ee52aeeb3b4d0c17c1f3f801cfb98
NucyLoodle <lucymeredith3@gmail.com>
2025-04-01 15:25:13 +0100

    add logout functionality

--- app/(auth)/(tabs)/index.tsx
+++ app/(auth)/(tabs)/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Pressable } from "react-native";
 import { Link } from "expo-router";
+import { useAuth } from "@/context/AuthContext";
 
 export default function Index() {
   // logged in users can see this page
+  const { logout} = useAuth();
   return (
     <View
       style={{
@@ -14,6 +16,8 @@ export default function Index() {
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
       <Link href="/(auth)/create">Create Something</Link>
+      <Link href="/(auth)/TrackVan">Start Tracking</Link>
+      <Pressable onPress={logout}><Text>Logout</Text></Pressable>
     </View>
   );
 }
