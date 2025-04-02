3e96b3fe9df8f973b01d8a04a84b89bd669da4f3
NucyLoodle <lucymeredith3@gmail.com>
2025-04-02 20:14:34 +0100

    fetch name for welcome message

--- app/(auth)/(tabs)/index.tsx
+++ app/(auth)/(tabs)/index.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View, Pressable } from "react-native";
 import { Link } from "expo-router";
 import { useAuth } from "@/context/AuthContext";
+import * as SecureStore from 'expo-secure-store';
 
 export default function Index() {
   // logged in users can see this page
+  const [name, setName] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchName() {
+      const storedName = await SecureStore.getItemAsync("firstName");
+      setName(storedName);
+    }
+    fetchName();
+  }, []);
+
+
   const { logout} = useAuth();
   return (
     <View
@@ -14,9 +26,11 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Link href="/(auth)/create">Create Something</Link>
-      <Link href="/(auth)/TrackVan">Start Tracking</Link>
+      <Text>Welcome {name}!</Text>
+      <Text>What would you like to do today?</Text>
+      <Link href="/(auth)/create">View Vans</Link>
+      <Link href="/(auth)/create">View Drivers</Link>
+      <Link href="/(auth)/TrackVan">Share My Location</Link>
       <Pressable onPress={logout}><Text>Logout</Text></Pressable>
     </View>
   );
