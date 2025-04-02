a000d2d0bd13a7409e348f65859051a65494c89b
NucyLoodle <lucymeredith3@gmail.com>
2025-04-02 14:53:46 +0100

    fix tabs

--- app/(public)/_layout.tsx
+++ app/(public)/_layout.tsx
@@ -3,19 +3,21 @@ import { Redirect, Stack } from "expo-router";
 import { useAuth } from "../../context/AuthContext";
 
 export default function PublicLayout() {
-  const { isAuthenticated, loading } = useAuth();
+	const { isAuthenticated, loading } = useAuth();
 
-  if (loading) return null; // Prevent flickering while checking auth
+	if (loading) return null; // Prevent flickering while checking auth
 
-  if (isAuthenticated) {
-    return <Redirect href="/(auth)/(tabs)" />; // Redirect logged-in users
-  }
+	if (isAuthenticated) {
+		return <Redirect href="/(auth)/(tabs)" />; // Redirect logged-in users
+	}
 
-  return <Stack>
-          <Stack.Screen name="Login" options={{headerShown: false}}/>
-          <Stack.Screen name="createInvite" options={{headerShown: false}}/>
-          <Stack.Screen name="Verify" options={{headerShown: false}}/>
-          <Stack.Screen name="Home" options={{headerShown: false}}/>
-        </Stack>;
+	return (
+		<Stack>
+			<Stack.Screen name="Login" options={{headerShown: false}}/>
+			<Stack.Screen name="createInvite" options={{headerShown: false}}/>
+			<Stack.Screen name="Verify" options={{headerShown: false}}/>
+			<Stack.Screen name="Home" options={{headerShown: false}}/>
+		</Stack>
+	)
 }
 
