3cfad91f35f5fa800a46796b2d8304513aafb59d
NucyLoodle <lucymeredith3@gmail.com>
2025-04-02 14:47:51 +0100

    configure splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import * as SplashScreen from "expo-splash-screen";
+import React, { useEffect } from "react";
 import { Stack } from "expo-router";
 import { AuthProvider } from "@/context/AuthContext";
+import { useFonts,AlfaSlabOne_400Regular, } from "@expo-google-fonts/alfa-slab-one";
+import { Poppins_400Regular } from "@expo-google-fonts/poppins";
+
+
+SplashScreen.preventAutoHideAsync();
+
+SplashScreen.setOptions({
+      duration: 1000,
+      fade: true,
+});
 
 
 export default function RootLayout() {
+    const [fontsLoaded] = useFonts({ AlfaSlabOne_400Regular, Poppins_400Regular, });
+
+    useEffect(() => {
+        if (fontsLoaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+
+
     return (
         <AuthProvider>
             <Stack>
@@ -13,5 +38,4 @@ export default function RootLayout() {
             </Stack>
         </AuthProvider>
     )
-
 }
