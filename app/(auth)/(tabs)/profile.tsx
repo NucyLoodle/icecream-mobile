8181e91407195c7b06a96d3354395454e1cf195f
NucyLoodle <lucymeredith3@gmail.com>
2025-04-07 20:20:26 +0100

    create basic profile

--- app/(auth)/(tabs)/profile.tsx
+++ app/(auth)/(tabs)/profile.tsx
@@ -1,11 +1,41 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Profile() {
-  // logged in users can see this page
-  return (
-    <View>
-      <Text>Profile</Text>
-    </View>
-  );
+	// logged in users can see this page
+	return (
+		<SafeAreaView style={styles.container}>
+			<Text style={styles.heading}>Your Details</Text>
+			<View>
+				<Text style={styles.text}>Company Name: </Text>
+				<Text style={styles.text}>First Name: </Text>
+				<Text style={styles.text}>Last Name: </Text>
+				<Text style={styles.text}>Company Website: </Text>
+				<Text style={styles.text}>Contact Email: </Text>
+				<Text style={styles.text}>Contact Phone: </Text>
+			</View>
+			<Text style={styles.text}>Contact us to update your info</Text>
+		</SafeAreaView>
+	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#eab2bb",
+		alignItems: "center",
+		justifyContent: "center",
+		padding: 5,
+	},
+	heading: {
+		color: "#3c6ca8",
+		fontFamily: "AlfaSlabOne_400Regular",
+		fontSize: 20,
+	},
+	text: {
+		fontFamily: "Poppins_400Regular",
+		color: '#3e1755',
+		fontSize: 20,
+	},
+});
