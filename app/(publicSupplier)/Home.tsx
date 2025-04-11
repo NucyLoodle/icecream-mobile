badc1027224ab45aea54b6da38d455826f3dfe2a
NucyLoodle <lucymeredith3@gmail.com>
2025-04-11 13:14:47 +0100

    remove deep link functionality

--- app/(publicSupplier)/Home.tsx
+++ app/(publicSupplier)/Home.tsx
@@ -7,37 +7,37 @@ import * as Linking from "expo-linking"
 export default function Home() {
     // welcome page for logged out users
   const router = useRouter();
-  const [inviteToken, setInviteToken] = useState<string | null>(null);
+//   const [inviteToken, setInviteToken] = useState<string | null>(null);
 
-  useEffect(() => {
-    const handleDeepLink = (event: Linking.EventType) => {
-      const url = event.url;
-      const parsed = Linking.parse(url);
-      if (parsed.queryParams?.token) {
-        const token = parsed.queryParams.token as string;
-        setInviteToken(token);
-        Alert.alert("Invite Token Received", `Token: ${token}`);
+//   useEffect(() => {
+//     const handleDeepLink = (event: Linking.EventType) => {
+//       const url = event.url;
+//       const parsed = Linking.parse(url);
+//       if (parsed.queryParams?.token) {
+//         const token = parsed.queryParams.token as string;
+//         setInviteToken(token);
+//         Alert.alert("Invite Token Received", `Token: ${token}`);
 
-        // Navigate to the verify screen and pass the token
-        router.push({
-          pathname: "/(publicSupplier)/Verify",
-          params: { token }, // Pass token as query param
-        });
-      }
-    };
+//         // Navigate to the verify screen and pass the token
+//         router.push({
+//           pathname: "/(publicSupplier)/Verify",
+//           params: { token }, // Pass token as query param
+//         });
+//       }
+//     };
 
-    // Listen for deep links
-    const subscription = Linking.addEventListener("url", handleDeepLink);
+//     // Listen for deep links
+//     const subscription = Linking.addEventListener("url", handleDeepLink);
 
-    // Check if the app was opened via a deep link
-    Linking.getInitialURL().then((url) => {
-      if (url) handleDeepLink({ url });
-    });
+//     // Check if the app was opened via a deep link
+//     Linking.getInitialURL().then((url) => {
+//       if (url) handleDeepLink({ url });
+//     });
 
-    return () => {
-      subscription.remove();
-    };
-  }, []);
+//     return () => {
+//       subscription.remove();
+//     };
+//   }, []);
 
   return (
 
@@ -56,7 +56,8 @@ export default function Home() {
 				</Pressable>
 
 				<Pressable
-					onPress={() => router.push({pathname: "/(publicSupplier)/Verify", params: inviteToken ? { token: inviteToken } : undefined})}
+					// onPress={() => router.push({pathname: "/(publicSupplier)/Verify", params: inviteToken ? { token: inviteToken } : undefined})}
+					onPress={() => router.push({pathname: "/(publicSupplier)/Verify"})}
 					style={({pressed}) => [
 						{
 						backgroundColor: pressed ? '#eee060' : '#b8ecce',
