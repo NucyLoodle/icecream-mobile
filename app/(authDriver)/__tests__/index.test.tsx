f2cc055825fab6d804991e8df23b06fc950e2c40
NucyLoodle <lucymeredith3@gmail.com>
2025-04-23 14:19:04 +0100

    write tests for index screen

---
+++ app/(authDriver)/__tests__/index.test.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react-native';
+import Index from '../index';
+import * as SecureStore from 'expo-secure-store';
+import { AuthProvider } from '@/context/AuthContext';
+import { getPressableStyle } from '../index';
+import { useRouter } from 'expo-router'; 
+
+const mockPush = jest.fn();
+
+jest.mock('expo-router', () => ({
+  useRouter: () => ({
+    push: mockPush,
+  }),
+}));
+
+jest.mock('expo-secure-store', () => ({
+    getItemAsync: jest.fn(),
+}));
+
+describe('Index Component', () => {
+
+    it('renders correctly', async () => {
+        (SecureStore.getItemAsync as jest.Mock).mockImplementation(async (key) => {
+            if (key === 'firstName') return 'Mr Frost';
+            return null;
+        });
+          
+        const { getByText } = render(
+            <AuthProvider>
+                <Index />
+            </AuthProvider>);
+
+        await waitFor(() => {
+            expect(getByText('Welcome, Mr Frost!')).toBeTruthy();
+        });
+    });
+
+    it('applies the correct backgroundColor based on pressed state', () => {
+        expect(getPressableStyle(true).backgroundColor).toBe('#b8ecce');
+        expect(getPressableStyle(false).backgroundColor).toBe('#eab2bb');
+    });	
+    
+    it('navigates to ChooseVan screen when "Share my Location" is pressed', async () => {
+        (SecureStore.getItemAsync as jest.Mock).mockImplementation(async (key) => {
+            if (key === 'firstName') return 'Mr Frost';
+            return null;
+        });
+    
+        const { getByText } = render(
+            <AuthProvider>
+                <Index />
+            </AuthProvider>
+        );
+    
+        await waitFor(() => {
+            expect(getByText('Welcome, Mr Frost!')).toBeTruthy();
+        });
+    
+        const shareButton = getByText('Share my Location');
+    
+        fireEvent.press(shareButton);
+    
+        expect(mockPush).toHaveBeenCalledWith('/(auth)/ChooseVan');
+    });
+    
+
+});
